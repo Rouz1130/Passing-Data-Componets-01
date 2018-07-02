516894b14e0ce0abb1d6d09c14152790e6c3f82b
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-02 02:08:31 -0700

    Uncomment Menu component for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { RouterModule } from '@angular/router';
 
 import {MatButtonModule, MatCheckboxModule, MatChipsModule, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
-// import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './menu/menu.component';
 
 
-
-// const routes = [
-//   {path:'', component: AppComponent},
-//   {path:'child-parent', component: ChildParentComponent},
-// ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
-    // MenuComponent,
+    MenuComponent,
 
   ],
   imports: [
@@ -30,8 +24,7 @@ import { ChildComponent } from './child/child.component';
     MatButtonModule,
     MatCheckboxModule, 
     MatChipsModule,
-    MatToolbarModule,
-    // RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
