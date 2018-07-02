3fda6ed67a817e787f6b10c504692b8746794b73
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-02 00:49:11 -0700

    Add OnChanges lifeCycle hook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import { RouterModule } from '@angular/router';
+
 import {MatButtonModule, MatCheckboxModule, MatChipsModule, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
-
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MenuComponent } from './menu/menu.component';
 
+
+// const routes = [
+
+//   {pth:''  },
+// ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
-    NavBarComponent,
     MenuComponent
   ],
   imports: [
@@ -25,7 +28,8 @@ import { MenuComponent } from './menu/menu.component';
     MatButtonModule,
     MatCheckboxModule, 
     MatChipsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    // RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
-// import { Input } from '@angular/core/src/metadata/directives';
+// 1: importing required libraries for onChanges lifecycle hook:  OnChanges, SimpleChanges, SimpleChange 
+import { Component, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+
 
 @Component({
   selector: 'child-component',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
 
+// 2: modififed class to implement interface OnChanges
+export class ChildComponent implements OnChanges {
  @Input() count: number;
+
+
+ // 3: ngOnChanges method.
+ ngOnChanges(changes: SimpleChanges) {
+
+  for (let property in changes) {
+    if (property === 'count') {
+      console.log('Previous:', changes[property].previousValue);
+      console.log('Current:', changes[property].currentValue);
+      console.log('firstChange', changes[property].firstChange);
+    }
+  }
+
+ }
 
 }
